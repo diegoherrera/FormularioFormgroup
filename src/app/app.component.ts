39206137a8ebdcd16511758fb09d0c50b1a69be4
Diego Sebastian Herrera <diherrer@everis.com>
2020-06-24 20:45:17 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Actor } from './Entidades/Actor';
+import { NgForm, FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,34 @@ import { Actor } from './Entidades/Actor';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'router00';
+  title = 'Form';
+  userForm = new FormGroup({
+    Nombre : new FormControl('', [Validators.required, Validators.maxLength(15)]),
+    Apellido : new FormControl('', [Validators.required, Validators.maxLength(55)]),
+    Edad : new FormControl(20, Validators.required),
+    Pais : new FormControl({ value: 'Argentina', disabled: true })
+  });
 
-  
 
   constructor() {
-    
   }
+
+  setAllValue() {
+    this.userForm.setValue({Nombre: 'Jose ', Apellido : 'Perez', Edad: '23', Pais: 'Australia' }); 
+  }
+
+  setOneValue() {
+    this.userForm.patchValue({Nombre: 'Diego Sebastian '}); 
+  }
+
+  resetForm() { 
+    this.userForm.reset({Nombre: '', Apellido : '', Edad: '', Pais: '' });
+ } 
+
+  onFormSubmit() {
+    console.log(this.userForm);
+    console.log('Nombre:' + this.userForm.get('Nombre').value);
+    console.log('Apellido:' + this.userForm.get('Apellido').value);
+  }
+  
 }
